7f88dacb6f873d23c6568c0b8202532fb6bd02a3
Michał Kalbarczyk <fazibear@gmail.com>
2015-09-27 17:10:22 +0200

    use url_for router method

--- app/components/navbar.rb
+++ app/components/navbar.rb
@@ -11,22 +11,22 @@ class NavBar
             end
             ul class: 'nav navbar-nav' do
               li class: "#{"active" if router.path == '/'}" do
-                a href: '/' do
+                a href: router.url_for(:home) do
                   text 'Home'
                 end
               end
               li class: "#{"active" if router.path == '/welcome'}" do
-                a href: '/welcome' do
+                a href: router.url_for(:welcome) do
                   text 'Welcome'
                 end
               end
               li class: "#{"active" if router.path == '/goodbye'}" do
-                a href: '/goodbye' do
+                a href: router.url_for(:goodbye) do
                   text 'Goodbye'
                 end
               end
               li class: "#{"active" if router.path == '/counter'}" do
-                a href: '/counter' do
+                a href: router.url_for(:counter) do
                   text 'Counter'
                 end
               end
