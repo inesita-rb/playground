c2f3256166ccf4826f062282e937c3ea1416954b
Michał Kalbarczyk <fazibear@gmail.com>
2018-08-18 18:12:23 +0200

    Add files

---
+++ app/application.rb
@@ -0,0 +1,34 @@
+require 'inesita'
+require 'inesita-router'
+
+require 'browser'
+require 'browser/socket'
+require 'browser/interval'
+require 'browser/http'
+
+require_tree './stores'
+require 'store'
+
+require 'router'
+require_tree './components'
+
+# fix headers
+Browser::HTTP::Request::HEADERS.delete('X-Opal-Version')
+
+class Application
+  include Inesita::Component
+
+  inject Store
+  inject Router
+
+  def render
+    div class: 'container' do
+      component NavBar
+      component router
+    end
+  end
+end
+
+$document.ready do
+  Application.mount_to($document.body)
+end
